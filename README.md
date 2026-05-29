60282bc7a2bddb5d3ae0ba1f3ae6a4d1abbd3094
therealjameswilson <30484292+therealjameswilson@users.noreply.github.com>
2026-05-29 07:02:58 -0400

    Create FRUS assist page

---
+++ README.md
@@ -0,0 +1,10 @@
+# FRUS Assist - 1989-1992, Volume XX, North Africa; Sub-Saharan Africa
+
+Compiler-facing static workspace for *Foreign Relations of the United States, 1989-1992, Volume XX, North Africa; Sub-Saharan Africa*.
+
+- Official volume page: https://history.state.gov/historicaldocuments/frus1989-92v20
+- GitHub Pages target: https://therealjameswilson.github.io/frus-1989-92v20-assist/
+- Status source: https://history.state.gov/historicaldocuments/status-of-the-series
+- NARA Scout query: https://therealjameswilson.github.io/nara-scout/#q=North+OR+Africa+OR+Sub-Saharan&from=1989&to=1992&sort=relevance&perColl=25&perPage=50
+
+This is a lightweight starter site generated for volumes marked **Being Researched** by the Office of the Historian.
---
+++ data/volume.js
@@ -0,0 +1,15 @@
+window.FRUS_VOLUME = {
+  "index": 20,
+  "doc_id": "frus1989-92v20",
+  "title": "1989\u20131992, Volume XX, North Africa; Sub-Saharan Africa",
+  "official_url": "https://history.state.gov/historicaldocuments/frus1989-92v20",
+  "repo_slug": "frus-1989-92v20-assist",
+  "title_ascii": "1989-1992, Volume XX, North Africa; Sub-Saharan Africa",
+  "pages_url": "https://therealjameswilson.github.io/frus-1989-92v20-assist/",
+  "repo_url": "https://github.com/therealjameswilson/frus-1989-92v20-assist",
+  "nara_scout_url": "https://therealjameswilson.github.io/nara-scout/#q=North+OR+Africa+OR+Sub-Saharan&from=1989&to=1992&sort=relevance&perColl=25&perPage=50",
+  "companion": [
+    "North Africa and Sub-Saharan Africa assistant",
+    "https://therealjameswilson.github.io/NorthAfrica-89-92/"
+  ]
+};
